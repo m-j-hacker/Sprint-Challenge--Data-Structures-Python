6d928fd67414d629842d6e5347c7a0433e0276da
m-j-hacker <mjhacker@gmail.com>
2019-05-17 10:32:43 -0500

    Begin writing out ring buffer logic

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,17 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    # Adds an element to the end of a buffer.
+    # If the buffer is full, overwrite the oldest data
+    if len(self.storage) == self.capacity:
+      self.storage[self.current] = item
+    # If the buffer is not full, append the item and then check if the
+    # buffer has been filled
+    else:
+      self.storage.append(item)
+      if len(self.storage) == self.capacity:
+        self.current = 0
 
   def get(self):
-    pass
+    if len(self.storage) == self.capacity:
+      self.storage[self.current:]+self.storage[:self.current]
