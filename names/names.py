ab085b70dab1832ae10c8c68c8631cdb13fbbb0b
m-j-hacker <mjhacker@gmail.com>
2019-05-17 12:25:23 -0500

    Progress: implement BST in names, DS answers

--- names/names.py
+++ names/names.py
@@ -1,5 +1,45 @@
 import time
 
+class BinarySearchTree:
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+
+  def insert(self, value):
+    # first, we need to determine if the value is less or greater than self.value
+
+    # If value is less than and there is not already a value on the left, assign value to left
+    if value < self.value:
+
+      if not self.left:
+        self.left = BinarySearchTree(value)
+    # If there is a value on the left already, run insert recursively
+      else:
+        self.left.insert(value)
+
+    # If value is greater than or equal to self.value, assign value to right
+    else:
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
+
+  def contains(self, target):
+    # Here we need to determine if the target can be found in the search tree
+    if target == self.value:
+      return self.value
+    if target < self.value:
+      if not self.left:
+        return False
+      else:
+        return self.left.contains(target)
+    else:
+      if not self.right:
+        return False
+      else:
+        return self.right.contains(target)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -19,4 +59,9 @@ for name_1 in names_1:
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+# We want to improve this runtime.
+# We can do this with a binary search
+
+def search()
 
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,24 +1,54 @@
+# New strategy: I need to create a flag that permanently marks
+# The ring buffer as full when it fills up, this will simplify
+# the logic that is required
+
 class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
     self.current = 0
     self.storage = [None]*capacity
 
-  def append(self, item):
-    # Adds an element to the end of a buffer.
-    # If the buffer is full, overwrite the oldest data
-    if len(self.storage) == self.capacity:
+  class __Full:
+    def append(self, item):
+      # Adds an element to the end of the buffer.
+      # If the buffer is full, overwrite the oldest data
       self.storage[self.current] = item
       self.current = (self.current + 1) % self.capacity
+    def get(self):
+      # The list should be returned in the right order, from oldest to newest
+      return self.storage[self.current:]+self.storage[:self.current]
+
+  def append(self, item):
+    
+    self.storage[self.current] = item
     # If the buffer is not full, append the item and then check if the
     # buffer has been filled
+    if self.current == self.capacity - 1:
+      self.current = 0
+      self.__class__ = self.__Full
     else:
-      self.storage.append(item)
-      if len(self.storage) == self.capacity:
-        self.current = 0
+      self.current += 1
 
   def get(self):
-    if len(self.storage) == self.capacity:
-      return self.storage[self.current:]+self.storage[:self.current]
-    else:
-      return self.storage
+    array = []
+    for i in range(len(self.storage)):
+      if self.storage[i] is None:
+        continue
+      else:
+        array.append(self.storage[i])
+
+    return array
+    # array_to_print = []
+    # for i in self.storage:
+    #   if self.storage[i] != None:
+    #     array_to_print[i] = self.storage[i]
+    # # if len(self.storage) == self.capacity:
+    # #   return self.storage[self.current:]+self.storage[:self.current]
+    
+    #   return array_to_print
+    # else:
+    # print_array = []
+    # for i in range(0, self.capacity - 1):
+    #   if self.storage[i]:
+    #     print_array[i] = self.storage[i]
+    # return print_array
